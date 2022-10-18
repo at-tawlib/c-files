20d0b477d45c46ffb2603e76e44c032fdffd749a
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-10-18 08:59:44 +0000

    Add _print_number line

--- custom-printf/functions.c
+++ custom-printf/functions.c
@@ -12,3 +12,38 @@ void _print_string(char *str)
 		_putchar(str[i]);
 	}
 }
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+void _print_number(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	if (n < 10)
+	{
+		_putchar(n + '0');
+	}
+	else if (n < 100)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n < 1000)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar((n % 100) % 10 + '0');
+	}
+	else if (n < 10000)
+	{
+		_putchar(n / 1000 + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n % 100) / 10 + '0');
+		_putchar((n % 100) % 10 + '0');
+	}
+}
